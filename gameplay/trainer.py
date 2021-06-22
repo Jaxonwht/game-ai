b3c1abcba9e11d0dfe01da785737b7a538197e56
Haotian Wang <haotian@duolingo.com>
2021-06-21 23:47:43 -0400

    multiprocess staticmethod instead of instance method

--- gameplay/trainer.py
+++ gameplay/trainer.py
@@ -17,31 +17,41 @@ class GameTrainer:
         self.game: Game = game
         self.device = device
 
-    def _one_iteration(self, _x) -> Tuple[List[torch.Tensor], List[torch.Tensor], int]:
+    @staticmethod
+    def _one_iteration(args) -> Tuple[List[torch.Tensor], List[torch.Tensor], int]:
+        game, model, device, train_playout_times, search_depth_cap = args
         empirical_p_list: List[torch.Tensor] = []
         state_list: List[torch.Tensor] = []
-        self.game.start()
 
         with torch.no_grad():
-            mcts = MCTSController(self.game, self.model, self.device)
-            while not self.game.over:
-                mcts.simulate(self.config.train_playout_times, self.config.search_depth_cap)
+            mcts = MCTSController(game, model, device)
+            while not game.over:
+                mcts.simulate(train_playout_times, search_depth_cap)
                 empirical_p_list.append(mcts.empirical_probability)
-                state_list.append(self.game.game_state)
+                state_list.append(game.game_state)
 
                 sampled_move: int = int(mcts.empirical_probability.multinomial(1).item())
-                self.game.make_move(sampled_move)
+                game.make_move(sampled_move)
                 mcts.confirm_move(sampled_move)
 
-        return state_list, empirical_p_list, self.game.score
+        return state_list, empirical_p_list, game.score
 
     def train(self) -> None:
         game_count = 0
         for _ in range(self.config.train_iterations):
+            self.game.start()
             with mp.Pool() as pool:
                 iterator = pool.imap_unordered(
-                    self._one_iteration,
-                    range(self.config.mcts_batch_size),
+                    GameTrainer._one_iteration,
+                    (
+                        (
+                            self.game,
+                            self.model,
+                            self.device,
+                            self.config.train_playout_times,
+                            self.config.search_depth_cap
+                        ) for _ in range(self.config.mcts_batch_size)
+                    ),
                     chunksize=self.config.mcts_batch_chunk_size
                 )
                 loss = self.model.train_game(*zip(*iterator))  # type: ignore
